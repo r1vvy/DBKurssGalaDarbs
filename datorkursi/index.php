8535986f7ced6423695c73ca71760cc42777e6e2
r1vvy <s20seflemil@vea.lv>
2023-01-10 08:49:30 +0200

    Add: schema's table listing to the homepage

--- datorkursi/index.php
+++ datorkursi/index.php
@@ -11,11 +11,11 @@
                     <div class="shadow p-3 mb-5 bg-body rounded">
                         <img src="atteli/shema.png" alt="Datubāzes shēma" class="img-fluid">
                     </div>
-                        
             </div>
             <div class="col-sm-6">
                 <h1 class="display-3 text-center mb-5">Apraksts</h1>
-                <p class="fw-normal font-monospace text-center text-md-start">
+                <?php include('tabulu_saraksts.php'); ?>
+                <p class="fw-normal font-monospace text-center text-md-start mt-5">
                         Izveidotā datubāze “kajasbumba” ir paredzēta futbola līgu un spēlētāju statistikas datu glabāšanai. 
                     Tai pieder 7 entītijas – “treneri”, “stadioni”, “speletaji”, “speletajs_statistika”, “klubi”, “ligas” un “ligas_klubi”. Entītijas – “speletaji”, “treneri” un “stadioni” tiek savienoti ar “klubi”. 
                     Vienam klubam var piesaistīt vairākus spēlētājus, vienu treneri (trenerim vienu klubu) un vienam stadionam vairākus klubus. 
@@ -23,6 +23,7 @@
                     Tā kā klubs var spēlēt vairākās līgās vienlaicīgi, bet spēlētājam katrā līgā statistika atšķirties, spēlētāju statistika tiek saglabāta katrai līgai norādot līgas nosaukumu (“Ligas_nosaukums”) un sezonu (“Sezona”). 
                     Šādi tiek nodrošināts, lai neveidojas datubāzē cikls (klubi > speletajs > statistika > liga > ligas_klubi > klubi) un 1 spēlētājam var piesaistīt statistiku no vairākām līgām.
                 </p>
+                
             </div>
         </div>
     </div>
--- datorkursi/tabulu_saraksts.php
+++ datorkursi/tabulu_saraksts.php
@@ -1,17 +1,12 @@
 <?php
     require_once("config.php");
-    include('head.php');
-    include('nav.php');
     $query = 'show tables';
     $result=$conn->query($query);
-    //$row = $result->fetch_assoc();
-    //print_r($row);
 
-    //echo '<th>header</th>';
-    $i = 1;
+    echo '<ul class="list-group list-group-flush list-group-numbered">';
     while ($row = $result->fetch_assoc())
     {
-        echo $i.". ".$row["Tables_in_kajasbumba"].'<br/>';
-        $i++;
+        echo '<li class="list-group-item">'.$row["Tables_in_kajasbumba"].'</li>';
     }
+    echo '</ul>';
 ?>
