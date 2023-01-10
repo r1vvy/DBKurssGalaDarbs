e6fa3d0f3313c315b3da4a29fc65a03bdb63febf
r1vvy <s20seflemil@vea.lv>
2023-01-10 20:06:55 +0200

    Add: Styling to datu_ievietosanas_skripti; tabulu_izveides_skripti; vaicajumu_saraksts

--- datorkursi/tabulu_izveides_skripti.php
+++ datorkursi/tabulu_izveides_skripti.php
@@ -9,154 +9,170 @@
     </head>
     <body>
         <?php include('nav.php'); ?>
-        <h4>Tabula "stadioni"</h4>
-        <pre>
-            CREATE TABLE IF NOT EXISTS `kajasbumba`.`stadioni` (
-            `StadionsID` INT(11) NOT NULL AUTO_INCREMENT,
-            `Nosaukums` VARCHAR(30) NOT NULL DEFAULT 'NONE',
-            `Pilseta` VARCHAR(30) NOT NULL DEFAULT 'NONE',
-            `Adrese` VARCHAR(400) NOT NULL,
-            `Ietilpiba` INT(10) NOT NULL,
-            `Segums` TINYTEXT NOT NULL DEFAULT 'NONE',
-            PRIMARY KEY (`StadionsID`))
-            ENGINE = InnoDB
-            AUTO_INCREMENT = 12
-            DEFAULT CHARACTER SET = utf8
-            COLLATE = utf8_bin;
-        </pre>
-        <h4>Tabula "treneri"</h4>
-        <pre>
-            CREATE TABLE IF NOT EXISTS `kajasbumba`.`treneri` (
-            `TrenerisID` INT(11) NOT NULL AUTO_INCREMENT,
-            `Uzvards` VARCHAR(45) NOT NULL DEFAULT 'NONE',
-            `Vards` VARCHAR(45) NOT NULL DEFAULT 'NONE',
-            `Valsts` VARCHAR(150) NOT NULL DEFAULT 'NONE',
-            PRIMARY KEY (`TrenerisID`))
-            ENGINE = InnoDB
-            AUTO_INCREMENT = 7
-            DEFAULT CHARACTER SET = utf8;
-        </pre>
-        <h4>Tabula "klubi"</h4>
-        <pre>
-            CREATE TABLE IF NOT EXISTS `kajasbumba`.`klubi` (
-                `KlubsID` INT(11) NOT NULL AUTO_INCREMENT,
-                `Nosaukums` VARCHAR(45) NOT NULL DEFAULT 'NONE',
-                `Abreviacija` TINYTEXT NOT NULL DEFAULT 'NONE',
-                `StadionsID` INT(11) NOT NULL,
-                `TrenerisID` INT(11) NOT NULL,
-                `Talrunis` INT(11) NOT NULL DEFAULT 0,
-                `Epasts` TINYTEXT NOT NULL DEFAULT 'NONE',
-                PRIMARY KEY (`KlubsID`),
-                UNIQUE INDEX `TrenerisID` (`TrenerisID` ASC) VISIBLE,
-                INDEX `StadionsID` (`StadionsID` ASC) VISIBLE,
-                CONSTRAINT `StadionsID`
-                    FOREIGN KEY (`StadionsID`)
-                    REFERENCES `kajasbumba`.`stadioni` (`StadionsID`)
-                    ON DELETE NO ACTION
-                    ON UPDATE CASCADE,
-                CONSTRAINT `TrenerisID`
-                    FOREIGN KEY (`TrenerisID`)
-                    REFERENCES `kajasbumba`.`treneri` (`TrenerisID`)
-                    ON DELETE NO ACTION
-                    ON UPDATE CASCADE)
-                ENGINE = InnoDB
-                AUTO_INCREMENT = 9
-                DEFAULT CHARACTER SET = utf8;
-        </pre>
-        <h4>Tabula "ligas"</h4>
-        <pre>
-            CREATE TABLE IF NOT EXISTS `kajasbumba`.`ligas` (
-            `LigaID` INT(11) NOT NULL AUTO_INCREMENT,
-            `Nosaukums` VARCHAR(20) NOT NULL DEFAULT 'NONE',
-            `Abreviacija` VARCHAR(5) NOT NULL DEFAULT 'NONE',
-            `Valsts` TINYTEXT NOT NULL DEFAULT 'NONE',
-            `Sezona` YEAR(4) NULL DEFAULT NULL,
-            `Sakums` DATE NULL DEFAULT NULL,
-            `Beigas` DATE NULL DEFAULT NULL,
-            PRIMARY KEY (`LigaID`))
-            ENGINE = InnoDB
-            AUTO_INCREMENT = 4
-            DEFAULT CHARACTER SET = utf8;
-        </pre>
-        <h4>Tabula "ligas_klubi"</h4>
-        <pre>
-            CREATE TABLE IF NOT EXISTS `kajasbumba`.`ligas_klubi` (
-                `SarakstsID` INT(11) NOT NULL AUTO_INCREMENT,
-                `LigasID` INT(11) NOT NULL,
-                `KlubsID` INT(11) NOT NULL,
-                `Uzvaras` TINYINT(4) NOT NULL DEFAULT 0,
-                `Neizskirti` TINYINT(4) NOT NULL DEFAULT 0,
-                `Zaudes` TINYINT(4) NOT NULL DEFAULT 0,
-                `Punkti` INT(11) NOT NULL DEFAULT 0,
-                `Vartu_attieciba` INT(11) NOT NULL DEFAULT 0,
-                `Vieta` INT(11) NOT NULL DEFAULT 0,
-                PRIMARY KEY (`SarakstsID`),
-                INDEX `KlubsID` (`KlubsID` ASC) VISIBLE,
-                INDEX `LigasID` (`LigasID` ASC) VISIBLE,
-                CONSTRAINT `KlubsID`
-                    FOREIGN KEY (`KlubsID`)
-                    REFERENCES `kajasbumba`.`klubi` (`KlubsID`)
-                    ON DELETE NO ACTION
-                    ON UPDATE CASCADE,
-                CONSTRAINT `LigasID`
-                    FOREIGN KEY (`LigasID`)
-                    REFERENCES `kajasbumba`.`ligas` (`LigaID`)
-                    ON DELETE NO ACTION
-                    ON UPDATE CASCADE)
-                ENGINE = InnoDB
-                AUTO_INCREMENT = 10
-                DEFAULT CHARACTER SET = utf8;
-        </pre>
-        <h4>Tabula "speletaji"</h4>
-        <pre>
-            CREATE TABLE IF NOT EXISTS `kajasbumba`.`speletaji` (
-            `SpeletajsID` INT(11) NOT NULL AUTO_INCREMENT,
-            `Uzvards` VARCHAR(100) NULL DEFAULT 'NONE',
-            `Vards` VARCHAR(100) NULL DEFAULT 'NONE',
-            `Dzimums` ENUM('M', 'F') NULL DEFAULT NULL,
-            `Dzimsanas_diena` DATE NOT NULL,
-            `Valsts` VARCHAR(100) NOT NULL DEFAULT 'NONE',
-            `Pozicija` ENUM('GK', 'DEF', 'MID', 'FWD') NOT NULL,
-            `KlubsID` INT(11) NULL DEFAULT NULL,
-            PRIMARY KEY (`SpeletajsID`),
-            INDEX `KlubsIDSpeletaji` (`KlubsID` ASC) VISIBLE,
-            CONSTRAINT `KlubsIDSpeletaji`
-                FOREIGN KEY (`KlubsID`)
-                REFERENCES `kajasbumba`.`klubi` (`KlubsID`)
-                ON DELETE NO ACTION
-                ON UPDATE CASCADE)
-            ENGINE = InnoDB
-            AUTO_INCREMENT = 169
-            DEFAULT CHARACTER SET = utf8;
-        </pre>
-        <h4>Tabula "speletajs_statistika"</h4>
-        <pre>
-            CREATE TABLE IF NOT EXISTS `kajasbumba`.`speletajs_statistika` (
-            `StatistikaID` INT(11) NOT NULL AUTO_INCREMENT,
-            `SpeletajsID` INT(11) NOT NULL,
-            `Ligas_nosaukums` VARCHAR(150) NOT NULL DEFAULT 'NONE',
-            `Sezona` VARCHAR(45) NOT NULL DEFAULT 'NONE',
-            `Nospeletas_minutes` INT(11) NOT NULL DEFAULT 0,
-            `Speles` INT(11) NOT NULL DEFAULT 0,
-            `Varti` INT(11) NOT NULL DEFAULT 0,
-            `Piespeles` INT(11) NOT NULL DEFAULT 0,
-            `Pendeles_iesistas` INT(11) NOT NULL DEFAULT 0,
-            `Pendeles_nesekmigas` INT(11) NOT NULL DEFAULT 0,
-            `Bezvartu_speles` INT(11) NOT NULL DEFAULT 0,
-            `Ielaisti_varti` INT(11) NOT NULL DEFAULT 0,
-            `Dzeltenas_kartites` INT(11) NOT NULL DEFAULT 0,
-            `Sarkanas_kartites` INT(11) NOT NULL DEFAULT 0,
-            PRIMARY KEY (`StatistikaID`),
-            INDEX `SpeletajsID` (`SpeletajsID` ASC) VISIBLE,
-            CONSTRAINT `SpeletajsID`
-                FOREIGN KEY (`SpeletajsID`)
-                REFERENCES `kajasbumba`.`speletaji` (`SpeletajsID`)
-                ON DELETE NO ACTION
-                ON UPDATE CASCADE)
-            ENGINE = InnoDB
-            AUTO_INCREMENT = 209
-            DEFAULT CHARACTER SET = utf8
-            COLLATE = utf8_bin;
-        </pre>
+        <div class="container">
+            <div class="shadow p-3 mb-5 bg-body rounded">
+                <h1 class="display-5 text-center">stadioni</h1>
+                <pre>
+                    CREATE TABLE IF NOT EXISTS `kajasbumba`.`stadioni` (
+                    `StadionsID` INT(11) NOT NULL AUTO_INCREMENT,
+                    `Nosaukums` VARCHAR(30) NOT NULL DEFAULT 'NONE',
+                    `Pilseta` VARCHAR(30) NOT NULL DEFAULT 'NONE',
+                    `Adrese` VARCHAR(400) NOT NULL,
+                    `Ietilpiba` INT(10) NOT NULL,
+                    `Segums` TINYTEXT NOT NULL DEFAULT 'NONE',
+                    PRIMARY KEY (`StadionsID`))
+                    ENGINE = InnoDB
+                    AUTO_INCREMENT = 12
+                    DEFAULT CHARACTER SET = utf8
+                    COLLATE = utf8_bin;
+                </pre>
+            </div>
+            <div class="shadow p-3 mb-5 bg-body rounded">
+                <h1 class="display-5 text-center">treneri</h1>
+                <pre>
+                    CREATE TABLE IF NOT EXISTS `kajasbumba`.`treneri` (
+                    `TrenerisID` INT(11) NOT NULL AUTO_INCREMENT,
+                    `Uzvards` VARCHAR(45) NOT NULL DEFAULT 'NONE',
+                    `Vards` VARCHAR(45) NOT NULL DEFAULT 'NONE',
+                    `Valsts` VARCHAR(150) NOT NULL DEFAULT 'NONE',
+                    PRIMARY KEY (`TrenerisID`))
+                    ENGINE = InnoDB
+                    AUTO_INCREMENT = 7
+                    DEFAULT CHARACTER SET = utf8;
+                </pre>
+            </div>
+            <div class="shadow p-3 mb-5 bg-body rounded">
+                <h1 class="display-5 text-center">klubi</h1>
+                <pre>
+                    CREATE TABLE IF NOT EXISTS `kajasbumba`.`klubi` (
+                        `KlubsID` INT(11) NOT NULL AUTO_INCREMENT,
+                        `Nosaukums` VARCHAR(45) NOT NULL DEFAULT 'NONE',
+                        `Abreviacija` TINYTEXT NOT NULL DEFAULT 'NONE',
+                        `StadionsID` INT(11) NOT NULL,
+                        `TrenerisID` INT(11) NOT NULL,
+                        `Talrunis` INT(11) NOT NULL DEFAULT 0,
+                        `Epasts` TINYTEXT NOT NULL DEFAULT 'NONE',
+                        PRIMARY KEY (`KlubsID`),
+                        UNIQUE INDEX `TrenerisID` (`TrenerisID` ASC) VISIBLE,
+                        INDEX `StadionsID` (`StadionsID` ASC) VISIBLE,
+                        CONSTRAINT `StadionsID`
+                            FOREIGN KEY (`StadionsID`)
+                            REFERENCES `kajasbumba`.`stadioni` (`StadionsID`)
+                            ON DELETE NO ACTION
+                            ON UPDATE CASCADE,
+                        CONSTRAINT `TrenerisID`
+                            FOREIGN KEY (`TrenerisID`)
+                            REFERENCES `kajasbumba`.`treneri` (`TrenerisID`)
+                            ON DELETE NO ACTION
+                            ON UPDATE CASCADE)
+                        ENGINE = InnoDB
+                        AUTO_INCREMENT = 9
+                        DEFAULT CHARACTER SET = utf8;
+                </pre>
+            </div>
+            <div class="shadow p-3 mb-5 bg-body rounded">
+                <h1 class="display-5 text-center">ligas</h1>
+                <pre>
+                    CREATE TABLE IF NOT EXISTS `kajasbumba`.`ligas` (
+                    `LigaID` INT(11) NOT NULL AUTO_INCREMENT,
+                    `Nosaukums` VARCHAR(20) NOT NULL DEFAULT 'NONE',
+                    `Abreviacija` VARCHAR(5) NOT NULL DEFAULT 'NONE',
+                    `Valsts` TINYTEXT NOT NULL DEFAULT 'NONE',
+                    `Sezona` YEAR(4) NULL DEFAULT NULL,
+                    `Sakums` DATE NULL DEFAULT NULL,
+                    `Beigas` DATE NULL DEFAULT NULL,
+                    PRIMARY KEY (`LigaID`))
+                    ENGINE = InnoDB
+                    AUTO_INCREMENT = 4
+                    DEFAULT CHARACTER SET = utf8;
+                </pre>
+            </div>
+            <div class="shadow p-3 mb-5 bg-body rounded">
+                <h1 class="display-5 text-center">ligas_klubi</h1>
+                <pre>
+                    CREATE TABLE IF NOT EXISTS `kajasbumba`.`ligas_klubi` (
+                        `SarakstsID` INT(11) NOT NULL AUTO_INCREMENT,
+                        `LigasID` INT(11) NOT NULL,
+                        `KlubsID` INT(11) NOT NULL,
+                        `Uzvaras` TINYINT(4) NOT NULL DEFAULT 0,
+                        `Neizskirti` TINYINT(4) NOT NULL DEFAULT 0,
+                        `Zaudes` TINYINT(4) NOT NULL DEFAULT 0,
+                        `Punkti` INT(11) NOT NULL DEFAULT 0,
+                        `Vartu_attieciba` INT(11) NOT NULL DEFAULT 0,
+                        `Vieta` INT(11) NOT NULL DEFAULT 0,
+                        PRIMARY KEY (`SarakstsID`),
+                        INDEX `KlubsID` (`KlubsID` ASC) VISIBLE,
+                        INDEX `LigasID` (`LigasID` ASC) VISIBLE,
+                        CONSTRAINT `KlubsID`
+                            FOREIGN KEY (`KlubsID`)
+                            REFERENCES `kajasbumba`.`klubi` (`KlubsID`)
+                            ON DELETE NO ACTION
+                            ON UPDATE CASCADE,
+                        CONSTRAINT `LigasID`
+                            FOREIGN KEY (`LigasID`)
+                            REFERENCES `kajasbumba`.`ligas` (`LigaID`)
+                            ON DELETE NO ACTION
+                            ON UPDATE CASCADE)
+                        ENGINE = InnoDB
+                        AUTO_INCREMENT = 10
+                        DEFAULT CHARACTER SET = utf8;
+                </pre>
+            </div>
+            <div class="shadow p-3 mb-5 bg-body rounded">
+                <h1 class="display-5 text-center">speletaji</h1>
+                <pre>
+                    CREATE TABLE IF NOT EXISTS `kajasbumba`.`speletaji` (
+                    `SpeletajsID` INT(11) NOT NULL AUTO_INCREMENT,
+                    `Uzvards` VARCHAR(100) NULL DEFAULT 'NONE',
+                    `Vards` VARCHAR(100) NULL DEFAULT 'NONE',
+                    `Dzimums` ENUM('M', 'F') NULL DEFAULT NULL,
+                    `Dzimsanas_diena` DATE NOT NULL,
+                    `Valsts` VARCHAR(100) NOT NULL DEFAULT 'NONE',
+                    `Pozicija` ENUM('GK', 'DEF', 'MID', 'FWD') NOT NULL,
+                    `KlubsID` INT(11) NULL DEFAULT NULL,
+                    PRIMARY KEY (`SpeletajsID`),
+                    INDEX `KlubsIDSpeletaji` (`KlubsID` ASC) VISIBLE,
+                    CONSTRAINT `KlubsIDSpeletaji`
+                        FOREIGN KEY (`KlubsID`)
+                        REFERENCES `kajasbumba`.`klubi` (`KlubsID`)
+                        ON DELETE NO ACTION
+                        ON UPDATE CASCADE)
+                    ENGINE = InnoDB
+                    AUTO_INCREMENT = 169
+                    DEFAULT CHARACTER SET = utf8;
+                </pre>
+            </div>
+            <div class="shadow p-3 mb-5 bg-body rounded">
+                <h1 class="display-5 text-center">speletajs_statistika</h1>
+                <pre>
+                    CREATE TABLE IF NOT EXISTS `kajasbumba`.`speletajs_statistika` (
+                    `StatistikaID` INT(11) NOT NULL AUTO_INCREMENT,
+                    `SpeletajsID` INT(11) NOT NULL,
+                    `Ligas_nosaukums` VARCHAR(150) NOT NULL DEFAULT 'NONE',
+                    `Sezona` VARCHAR(45) NOT NULL DEFAULT 'NONE',
+                    `Nospeletas_minutes` INT(11) NOT NULL DEFAULT 0,
+                    `Speles` INT(11) NOT NULL DEFAULT 0,
+                    `Varti` INT(11) NOT NULL DEFAULT 0,
+                    `Piespeles` INT(11) NOT NULL DEFAULT 0,
+                    `Pendeles_iesistas` INT(11) NOT NULL DEFAULT 0,
+                    `Pendeles_nesekmigas` INT(11) NOT NULL DEFAULT 0,
+                    `Bezvartu_speles` INT(11) NOT NULL DEFAULT 0,
+                    `Ielaisti_varti` INT(11) NOT NULL DEFAULT 0,
+                    `Dzeltenas_kartites` INT(11) NOT NULL DEFAULT 0,
+                    `Sarkanas_kartites` INT(11) NOT NULL DEFAULT 0,
+                    PRIMARY KEY (`StatistikaID`),
+                    INDEX `SpeletajsID` (`SpeletajsID` ASC) VISIBLE,
+                    CONSTRAINT `SpeletajsID`
+                        FOREIGN KEY (`SpeletajsID`)
+                        REFERENCES `kajasbumba`.`speletaji` (`SpeletajsID`)
+                        ON DELETE NO ACTION
+                        ON UPDATE CASCADE)
+                    ENGINE = InnoDB
+                    AUTO_INCREMENT = 209
+                    DEFAULT CHARACTER SET = utf8
+                    COLLATE = utf8_bin;
+                </pre>
+            </div>
+        </div>
     </body>
 </html>
--- datorkursi/vaicajumu_saraksts.php
+++ datorkursi/vaicajumu_saraksts.php
@@ -9,91 +9,117 @@
     </head>
     <body>
         <?php include('nav.php'); ?>
-        <h4>Vaicājums 1</h4>
-        <pre>
-            SELECT Uzvards, Vards, Pozicija, Nosaukums
-            FROM speletaji INNER JOIN klubi
-            USING (KlubsID)
-            ORDER BY klubi.Nosaukums;
-        </pre>
-        <h4>Vaicājums 2</h4>
-        <pre>
-            SELECT * FROM speletaji
-            WHERE YEAR(Dzimsanas_diena) &gt;= 2000 AND (Pozicija = "MID" OR Pozicija = "DEF");
-        </pre>
-        <h4>Vaicājums 3</h4>
-        <pre>
-            SELECT Valsts, COUNT(*)
-            FROM speletaji
-            GROUP BY Valsts
-            ORDER BY 2 DESC;
-        </pre>
-        <h4>Vaicājums 4</h4>
-        <pre>
-            SELECT *, DATE_FORMAT(FROM_DAYS(DATEDIFF(NOW(), Dzimsanas_diena)), '%Y') + 0 AS vecums
-            FROM speletaji
-            ORDER BY vecums DESC;
-        </pre>
-        <h4>Vaicājums 5</h4>
-        <pre>
-            SELECT Vards, Uzvards, Pozicija, Nosaukums, Nospeletas_minutes
-            FROM klubi JOIN speletaji JOIN speletajs_statistika
-            ON klubi.KlubsID = speletaji.KlubsID AND
-            speletaji.SpeletajsID = speletajs_statistika.SpeletajsID AND
-            speletajs_statistika.Ligas_nosaukums = "Premier League"
-            ORDER BY Nospeletas_minutes DESC;
-        </pre>
-        <h4>Vaicājums 6</h4>
-        <pre>
-            SELECT Nosaukums, ROUND(AVG(Nospeletas_minutes), 0) as "Videjais speles laiks starp komandas speletajiem (min)", Ligas_nosaukums
-            FROM speletajs_statistika, klubi
-            WHERE Ligas_nosaukums = "Premier League" AND Nosaukums = 
-                (
-                    SELECT Nosaukums 
-                    FROM klubi, speletaji 
-                    WHERE 
-                        speletaji.SpeletajsID = speletajs_statistika.SpeletajsID 
-                        AND speletaji.KlubsID = klubi.KlubsID
-                )
-            GROUP BY Nosaukums;
-        </pre>
-        <h4>Vaicājums 7</h4>
-        <pre>
-            SELECT Uzvards, Vards, Nosaukums, Ligas_nosaukums
-            FROM klubi JOIN speletaji JOIN speletajs_statistika
-            ON klubi.KlubsID = speletaji.KlubsID AND
-            speletaji.SpeletajsID = speletajs_statistika.SpeletajsID AND 
-            speletaji.Uzvards LIKE "A%" AND speletajs_statistika.Ligas_nosaukums = "Europa League";
-        </pre>
-        <h4>Vaicājums 8</h4>
-        <pre>
-            SELECT *
-            FROM klubi RIGHT JOIN ligas_klubi
-            ON klubi.KlubsID = ligas_klubi.KlubsID
-            WHERE LigasID = 1;
-        </pre>
-        <h4>Vaicājums 9</h4>
-        <pre>
-            SELECT COUNT(DISTINCT Valsts) AS "Parstavetas valstis" 
-            FROM speletaji, speletajs_statistika
-            WHERE speletaji.SpeletajsID = speletajs_statistika.SpeletajsID AND
-            speletajs_statistika.Ligas_nosaukums = "Premier League";
-        </pre>
-        <h4>Vaicājums 10</h4>
-        <pre>
-            SELECT sp.SpeletajsID, sp.Vards, sp.Uzvards, sp.Pozicija, sp_sk.Total_mins
-            FROM speletaji AS sp
-            JOIN (SELECT SpeletajsID, SUM(Nospeletas_minutes) AS Total_mins
-                    FROM speletajs_statistika
-                    GROUP BY SpeletajsID
-                    ) AS sp_sk ON sp.SpeletajsID = sp_sk.SpeletajsID;
-        </pre>
-        <form action="vaicajuma_rezultats.php" method="post">
-        <label for="ieraksts">Ievadiet vaicājumu:</label>
-        <br/>
-        <textarea name="ieraksts" required rows="4" cols="80"></textarea>
-        <br/>
-        <input type="submit" value="izpildit">
-        </form>
+        <div class="container-fluid">
+            <div class="row d-flex justify-content-center" style="justify-content: center">
+                <div class="col-sm-3 shadow p-3 mb-5 bg-body rounded mx-3">
+                    <h1 class="display-5 mb-3 text-center">Vaicājums 1</h1>
+                    <pre>
+                        SELECT Uzvards, Vards, Pozicija, Nosaukums
+                        FROM speletaji INNER JOIN klubi
+                        USING (KlubsID)
+                        ORDER BY klubi.Nosaukums;
+                    </pre>
+                </div>
+                <div class="col-sm-3 shadow p-3 mb-5 bg-body rounded mx-3">
+                    <h1 class="display-5 mb-3 text-center">Vaicājums 2</h1>
+                    <pre>
+                        SELECT * FROM speletaji
+                        WHERE YEAR(Dzimsanas_diena) &gt;= 2000 AND (Pozicija = "MID" OR Pozicija = "DEF");
+                    </pre>
+                </div>
+                <div class="col-sm-3 shadow p-3 mb-5 bg-body rounded mx-3">
+                    <h1 class="display-5 mb-3 text-center">Vaicājums 3</h1>
+                    <pre>
+                        SELECT Valsts, COUNT(*)
+                        FROM speletaji
+                        GROUP BY Valsts
+                        ORDER BY 2 DESC;
+                    </pre>
+                </div>
+                <div class="col-sm-3 shadow p-3 mb-5 bg-body rounded mx-3">
+                    <h1 class="display-5 mb-3 text-center">Vaicājums 4</h1>
+                    <pre>
+                        SELECT *, DATE_FORMAT(FROM_DAYS(DATEDIFF(NOW(), Dzimsanas_diena)), '%Y') + 0 AS vecums
+                        FROM speletaji
+                        ORDER BY vecums DESC;
+                    </pre>
+                </div>
+                <div class="col-sm-3 shadow p-3 mb-5 bg-body rounded mx-3">
+                    <h1 class="display-5 mb-3 text-center">Vaicājums 5</h1>
+                    <pre>
+                        SELECT Vards, Uzvards, Pozicija, Nosaukums, Nospeletas_minutes
+                        FROM klubi JOIN speletaji JOIN speletajs_statistika
+                        ON klubi.KlubsID = speletaji.KlubsID AND
+                        speletaji.SpeletajsID = speletajs_statistika.SpeletajsID AND
+                        speletajs_statistika.Ligas_nosaukums = "Premier League"
+                        ORDER BY Nospeletas_minutes DESC;
+                    </pre>
+                </div>
+                <div class="col-sm-3 shadow p-3 mb-5 bg-body rounded mx-3">
+                    <h1 class="display-5 mb-3 text-center">Vaicājums 6</h1>
+                    <pre>
+                        SELECT Nosaukums, ROUND(AVG(Nospeletas_minutes), 0) as "Videjais speles laiks starp komandas speletajiem (min)", Ligas_nosaukums
+                        FROM speletajs_statistika, klubi
+                        WHERE Ligas_nosaukums = "Premier League" AND Nosaukums = 
+                            (
+                                SELECT Nosaukums 
+                                FROM klubi, speletaji 
+                                WHERE 
+                                    speletaji.SpeletajsID = speletajs_statistika.SpeletajsID 
+                                    AND speletaji.KlubsID = klubi.KlubsID
+                            )
+                        GROUP BY Nosaukums;
+                    </pre>
+                </div>
+                <div class="col-sm-3 shadow p-3 mb-5 bg-body rounded mx-3">
+                    <h1 class="display-5 mb-3 text-center">Vaicājums 7</h1>
+                    <pre>
+                        SELECT Uzvards, Vards, Nosaukums, Ligas_nosaukums
+                        FROM klubi JOIN speletaji JOIN speletajs_statistika
+                        ON klubi.KlubsID = speletaji.KlubsID AND
+                        speletaji.SpeletajsID = speletajs_statistika.SpeletajsID AND 
+                        speletaji.Uzvards LIKE "A%" AND speletajs_statistika.Ligas_nosaukums = "Europa League";
+                    </pre>
+                </div>
+                <div class="col-sm-3 shadow p-3 mb-5 bg-body rounded mx-3">
+                    <h1 class="display-5 mb-3 text-center">Vaicājums 8</h1>
+                    <pre>
+                        SELECT *
+                        FROM klubi RIGHT JOIN ligas_klubi
+                        ON klubi.KlubsID = ligas_klubi.KlubsID
+                        WHERE LigasID = 1;
+                    </pre>
+                </div>
+                <div class="col-sm-3 shadow p-3 mb-5 bg-body rounded mx-3">
+                    <h1 class="display-5 mb-3 text-center">Vaicājums 9</h1>
+                    <pre>
+                        SELECT COUNT(DISTINCT Valsts) AS "Parstavetas valstis" 
+                        FROM speletaji, speletajs_statistika
+                        WHERE speletaji.SpeletajsID = speletajs_statistika.SpeletajsID AND
+                        speletajs_statistika.Ligas_nosaukums = "Premier League";
+                    </pre>
+                </div>
+                <div class="col-sm-3 shadow p-3 mb-5 bg-body rounded mx-3">
+                    <h1 class="display-5 mb-3 text-center">Vaicājums 10</h1>
+                    <pre>
+                        SELECT sp.SpeletajsID, sp.Vards, sp.Uzvards, sp.Pozicija, sp_sk.Total_mins
+                        FROM speletaji AS sp
+                        JOIN (SELECT SpeletajsID, SUM(Nospeletas_minutes) AS Total_mins
+                                FROM speletajs_statistika
+                                GROUP BY SpeletajsID
+                                ) AS sp_sk ON sp.SpeletajsID = sp_sk.SpeletajsID;
+                    </pre>
+                </div>
+                <div class="col-sm-9 shadow p-3 mb-5 bg-body rounded mx-3" style="justify-content: center">
+                    <form action="vaicajuma_rezultats.php" method="post">
+                        <label for="ieraksts" class="form-label">Ievadiet vaicājumu:</label>
+                        <br/>
+                        <textarea name="ieraksts" class="form-control" required rows="4" cols="80"></textarea>
+                        <br/>
+                        <button type="submit" class="btn btn-dark mx-2">Izpildīt</button>
+                    </form>
+                </div>
+            </div>
+        </div>
     </body>
 </html>
